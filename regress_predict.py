d337c2deb06b5548aba5cc0e89918ba17284a584
hoon_ock <jock@andrew.cmu.edu>
2024-03-08 06:52:24 +0000

    input update

--- regress_predict.py
+++ regress_predict.py
@@ -25,16 +25,12 @@ def predict_fn(data_loader, model, device):
 
 
 def run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug=False):  
-    # with open(os.path.join(pt_ckpt_dir_path, "clip.yml"), "r") as f:
-    #     config = yaml.safe_load(f)
-    # Hyperparameters and settings   
-    
-   
+    # Hyperparameters and settings      
     ############################################################################
     ckpt_name = pt_ckpt_dir_path.split('/')[-1]
     pt_ckpt_path = os.path.join(pt_ckpt_dir_path, "checkpoint.pt")
-    model_config_path = os.path.join(pt_ckpt_dir_path, "clip.yml") #config["model_config"]
-    train_config_path = os.path.join(pt_ckpt_dir_path, "regress_train.yml") #config["train_config"]
+    model_config_path = os.path.join(pt_ckpt_dir_path, "clip.yml") 
+    train_config_path = os.path.join(pt_ckpt_dir_path, "regress_train.yml") 
     ############################################################################
     device = "cuda" if torch.cuda.is_available() else "cpu"
     if debug:
@@ -55,7 +51,6 @@ def run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug=False):
         
     # Load tokenizer
     tokenizer = RobertaTokenizerFast.from_pretrained('roberta-base')
-    # breakpoint()
     # Initialize training dataset
     test_dataset = RegressionDataset(texts = df_test["text"].values,
                                       targets = df_test["target"].values,
@@ -65,8 +60,7 @@ def run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug=False):
     # Create training dataloader
     test_data_loader = DataLoader(test_dataset, batch_size = batch,
                                   shuffle = False, num_workers=1)
-
-    #breakpoint()    
+    
     # ===================== MODEL and TOKENIZER ===============================
     with open(model_config_path, "r") as f:
         model_config = yaml.safe_load(f)
@@ -78,7 +72,7 @@ def run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug=False):
     with open(train_config_path, "r") as f:
         train_config = yaml.safe_load(f)
     head = train_config["head"]
-    # breakpoint()
+    
     if head == "pooler":
         model = RegressionModel2(model_config).to(device)
     else:
@@ -89,15 +83,13 @@ def run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug=False):
     print(ckpt_name)
     state_dict = torch.load(pt_ckpt_path, map_location=device)['model_state_dict']
     model.load_state_dict(state_dict, strict=True) 
-    # breakpoint() 
     # ========================= PREDICTION ====================================
     predictions = predict_fn(test_data_loader, model, device)
     
     # ========================= SAVE PREDICTION ===============================
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
     save_path = os.path.join(save_path, f"ssl-reg-{ckpt_name}-{tag}-strc.pkl")
-    
-    # df_test['pred'] = predictions
-    # df_test.to_pickle(save_path) 
     
     ## save as dictionary where key is id in df_test, and values are predictions
     predictions_dict = dict(zip(df_test["id"].values, predictions))
@@ -107,27 +99,23 @@ def run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug=False):
 
 if __name__ == "__main__":
     import argparse
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--split", type=str, choices=["eval","test"], default="test")
-    parser.add_argument("--model", type=str, choices=["gnoc", "escn", "scn", "eqv2"], default="eqv2")
-    parser.add_argument("--debug", action="store_true")
-    parser.add_argument("--ckpt", type=str, default=None)
-    parser.add_argument("--train_type", type=str, choices=["direct_regress", "clip_regress"], default="clip_regress")
-    # parser.add_argument("--data_path", type=str, default=None)    
-    # parser.add_argument("--pt_ckpt_dir_path", type=str, default=None)
-    # parser.add_argument("--save_path", type=str, default=None)
-
+    from datetime import datetime
+    parser = argparse.ArgumentParser(description="Script to get embeddings.")
+    
+    parser.add_argument("--data_path", type=str, required=True, help="Path to the data file.")
+    parser.add_argument("--pt_ckpt_dir_path", type=str, required=True, help="Path to the pretrained checkpoint directory.")
+    parser.add_argument("--save_path", type=str, required=True, help="Path where to save the embeddings.")
+    parser.add_argument("--tag", type=str, default=datetime.now().strftime("%y%m%d_%H%M%S"), help="Tag for the run, defaults to current date and time if not provided.")
+    parser.add_argument("--debug", action="store_true", help="Enable debug mode if set.")
+    
     args = parser.parse_args()
-    split = args.split
-    model = args.model
-    ckpt = args.ckpt
-    train_type = args.train_type
+    
+    # Directly use the provided paths and tag
+    data_path = args.data_path
+    pt_ckpt_dir_path = args.pt_ckpt_dir_path
+    save_path = args.save_path
+    tag = args.tag
     debug = args.debug
-    # Conduct prediction
-    data_path = f"/home/jovyan/shared-scratch/jhoon/ocp2023/clip_data/oc20dense_{split}_{model}_relaxed.pkl"
-    pt_ckpt_dir_path = f"/home/jovyan/shared-scratch/jhoon/ocp2023/checkpoints/{train_type}/{ckpt}"
-    save_path = "/home/jovyan/shared-scratch/jhoon/ocp2023/results/predictions" 
-    tag = f"{split}-{model}"
     print("=============================================================")
     print(f"Making predictions for {split} split of {model} model")
     run_prediction(data_path, pt_ckpt_dir_path, save_path, tag, debug)
