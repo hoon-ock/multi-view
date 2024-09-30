987fefb1a34fd0f04987cca4681f269460007d51
hoon_ock <jock@andrew.cmu.edu>
2024-09-30 18:26:45 +0000

    regress_run update

--- regress_run.py
+++ regress_run.py
@@ -13,62 +13,6 @@ from transformers import RobertaTokenizerFast
 from datetime import datetime
 import torch.nn as nn
 
-
-
-# def train_batch(data,
-#                 model,
-#                 batch_count,
-#                 device,
-#                 loss_fn):
-#     token, target = data
-#     token['input_ids'] = token['input_ids'].to(device)
-#     token['attention_mask'] = token['attention_mask'].to(device)
-#     token['input_ids'] = token['input_ids'].squeeze(1)
-#     token['attention_mask'] = token['attention_mask'].squeeze(1)
-#     target = target.to(device)
-#     outputs = model(token)
-#     outputs = outputs.squeeze(1)
-#     loss = loss_fn(outputs, target)
-#     return loss, outputs
-
-# def load_pretrained_model(model,
-#                           pretrained_model):
-#     load_state = torch.load(pretrained_model) 
-#     model_state = model.state_dict()
-#     print(model_state)
-#     # exit()
-#     for name, param in load_state.items():
-#         # print(name)
-#         if name not in model_state:
-#             print('NOT loaded:', name)
-#             continue
-#         else:
-#             print('loaded:', name)
-#             if isinstance(param, nn.parameter.Parameter):
-#                 param = param.data
-#         model_state[name].copy_(param)
-#         print("Loaded pre-trained model with success.")
-#     return model
-
-
-# def load_clip_checkpoint(model, checkpoint_path, device):
-
-#     # Load the state_dict from the checkpoint file
-#     state_dict = torch.load(checkpoint_path, map_location=device)
-
-#     # Print missing keys
-#     missing_keys, unexpected_keys = model.load_state_dict(state_dict, strict=False)
-#     print(f"Missing keys: {missing_keys}")
-
-#     # Load state_dict while ignoring missing keys
-#     model_dict = model.state_dict()
-#     pretrained_dict = {k: v for k, v in state_dict.items() if k in model_dict}
-#     model_dict.update(pretrained_dict)
-#     model.load_state_dict(model_dict)
-    
-#     return model
-
-
 def train_fn(data_loader, model, optimizer, device, 
              scheduler, loss_fn, log_interval, debug=False,):
     
@@ -126,19 +70,17 @@ def run_regression(config_file):
     with open(config_file, "r") as f:
         config = yaml.safe_load(f)
     # Hyperparameters and settings   
+    # Note!
+    # RESUME_PATH is a path to the checkpoint to resume training
+    # PT_CKPT_PATH is a path to the clip-styled pre-trained model checkpoint
     RUN_NAME = config["run_name"]+datetime.now().strftime("_%m%d_%H%M")
     TRAIN_PATH = config["train_path"] 
     VAL_PATH = config["val_path"]
     CKPT_SAVE_DIR = os.path.join(config["ckpt_save_path"], RUN_NAME)
-
-    ############################################################################
     RESUME_PATH = config["resume_path"] if config.get("resume_path") else None
     RESUME_CONFIG = config["resume_config"] if config.get("resume_config") else None
-
     PT_CKPT_PATH = config["pt_ckpt_path"] if config.get("pt_ckpt_path") else None
-    # CATBERTA_CKPT_PATH = config["catberta_ckpt_path"] if config.get("catberta_ckpt_path") else None
     MODEL_CONFIG = config["model_config"]
-    ############################################################################
     HEAD = config["head"] if config.get("head") else "regress"
     DEVICE = config["device"]
     EPOCHS = config["num_epochs"]
@@ -157,7 +99,6 @@ def run_regression(config_file):
     if RESUME_PATH and RESUME_CONFIG:
         MODEL_CONFIG = RESUME_CONFIG
 
-
     print("=============================================================")
     print(f"{RUN_NAME} is launched")
     print("=============================================================")
@@ -172,9 +113,9 @@ def run_regression(config_file):
     print(f"Scheduler: {SCHD}")
     print(f"Loss function: {LOSS_FN}")
     print("=============================================================")
+
     if not DEBUG:
         wandb.init(project="clip-regress", name=RUN_NAME) 
-                   #,dir='/home/jovyan/shared-scratch/jhoon/ocp2023/log')
         
     # ========================= COPY CONFIG FILE ==============================
     if not os.path.exists(CKPT_SAVE_DIR):
@@ -197,13 +138,11 @@ def run_regression(config_file):
     # Initialize training dataset
     train_dataset = RegressionDataset(texts = df_train["text"].values,
                                       targets = df_train["target"].values,
-                                      chg_emb = df_train["chg_emb"].values,
                                       tokenizer = tokenizer,
                                       seq_len= tokenizer.model_max_length)
     # Initialize validation dataset
     val_dataset = RegressionDataset(texts = df_val["text"].values,
                                     targets = df_val["target"].values,
-                                    chg_emb = df_val["chg_emb"].values,
                                     tokenizer = tokenizer,
                                     seq_len= tokenizer.model_max_length)
     # Create training dataloader
@@ -231,19 +170,8 @@ def run_regression(config_file):
     elif RESUME_PATH:
         print('resume training from ', RESUME_PATH)
         state_dict = torch.load(RESUME_PATH, map_location=DEVICE)['model_state_dict']
-        # breakpoint()
         model.load_state_dict(state_dict, strict=False)
 
-    # elif CATBERTA_CKPT_PATH:
-    #     print('loading pretrained catberta from')
-    #     print(CATBERTA_CKPT_PATH)
-    #     breakpoint()
-    #     state_dict = torch.load(CATBERTA_CKPT_PATH, map_location=DEVICE)
-
-
-        
-    # if "debug" not in RUN_NAME:
-    #     wandb.watch(model, log="parameters")
     # ====================== OPTIMIZER AND SCHEDULER =========================
     if config.get("optimizer") == "AdamW":
         optimizer = torch.optim.AdamW(model.parameters(), lr=LR, weight_decay=0.01) #originally 1e-6
@@ -326,6 +254,5 @@ Val MAE = {round(val_mae,3)},")
 
 
 if __name__ == "__main__":
-  
     # Run the training loop
     run_regression("regress_train.yml") 
